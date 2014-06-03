5d429dd779b1485e05079549f60f2d8394b4af0d
Jacinda Zhong <jacindaz@gmail.com>
2014-06-03 13:33:30 -0400

    Creates random_card/suit methods, deal_card method
     - deal_card method uses random_card and random_suit methods
       to deal a card with a suit
     - uses random_number(card_or_suit), that takes in the array of
       card numbers or array of suits
     - takes the length of these arrays to determine what range of
       index to return for the random number
     - uses random number as index to pull the actual card number or suit

--- blackjack.rb
+++ blackjack.rb
@@ -3,18 +3,9 @@
 class Deck
 
   def initialize
-    @card_number = [1,2,3,4,5,6,7,8,9,10, "J", "Q", "K"]
+    @card_number = [1,2,3,4,5,6,7,8,9,10, "J", "Q", "K", "A"]
     @suit = ["♦", "♣", "♠", "♥"]
   end
-
-end
-
-
-#Card to represent an individual playing card.
-#This class should contain the suit and the value and
-#provide methods for determining what type of card it is
-#(e.g. face card or ace).
-class Card < Deck
 
   def two_cards(player_or_dealer)
     2.times do
@@ -26,15 +17,36 @@ class Card < Deck
     end
   end
 
-  def random_card_number
-    index = (@card_number.length) - 1
+end
+
+
+#Card to represent an individual playing card.
+#This class should contain the suit and the value and
+#provide methods for determining what type of card it is
+#(e.g. face card or ace).
+class Card < Deck
+
+  def random_number(card_or_suit)
+    index = (card_or_suit.length) - 1
     return (rand() * index).round
+  end
+
+  def random_card
+    @card_number[random_number(@card_number)]
+  end
+
+  def random_suit
+    @suit[random_number(@suit)]
+  end
+
+  def deal_card
+    "#{random_card}#{random_suit}"
   end
 
 end
 
 test = Card.new
-puts test.random_card_number
+puts test.deal_card
 
 
 
@@ -43,6 +55,8 @@ puts test.random_card_number
 #based on the cards that have been dealt.
 class Hand
 
+  def score
+  end
 
 end
 
